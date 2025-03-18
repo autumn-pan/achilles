1ac151708234eae1fa95bc42bc82e63d275aa72b
Autumn Pan <pan662607@gmail.com>
2025-03-18 16:48:47 -0400

    implement Symbol struct

--- src/parser/symbol_table/symbol_table.c
+++ src/parser/symbol_table/symbol_table.c
@@ -2,6 +2,40 @@
 #include <stdio.h>
 #include <string.h>
 #include "./symbol_table.h"
+#include "./hasher.c"
+
+
+typedef struct 
+{
+    unsigned long hash;
+    char * identifier;
+    enum Modifiers * modifiers;
+    char * type;
+    char * datatype;
+} Symbol;
+
+Symbol init_symbol(ASTNode * node)
+{
+    char * id = node->data.identifier;
+    char * type = type_to_string(node->type);
+    char * datatype = NULL;
+    char * key;
+
+    if(node->type == VARIABLE_DECL || node->type == FUNCTION_DECL)
+        datatype = node->data.IdentifierData.type;
+
+    if(node->type == FUNCTION_DECL)
+        key = key_function(node);
+    else if (node->type == VARIABLE_DECL)
+        key = key_variable(node);
+
+    unsigned long hash = hash(key, hash_limit);
+
+    Symbol symbol = (Symbol)malloc(sizeof(Symbol));
+    symbol.identifier = strdup(id);
+    symbol.type = strdup(type);
+    symbol.datatype = strdup(datatype);
+}
 
 char * key_function(ASTNode * node)
 {
