8ef86f77e65d5fd4a1d1f8a0b5f79f81f0caff97
Autumn Pan <pan662607@gmail.com>
2025-03-19 11:23:41 -0400

    fix issue #15

--- src/parser/symbol_table/symbol_table.c
+++ src/parser/symbol_table/symbol_table.c
@@ -4,7 +4,7 @@
 #include "./symbol_table.h"
 #include "./hash.c"
 
-
+// The symbol struct represents an individual entry in a symbol table
 typedef struct 
 {
     unsigned long hash;
@@ -12,8 +12,10 @@ typedef struct
     enum Modifiers * modifiers;
     char * type;
     char * datatype;
+    char * key;
 } Symbol;
 
+// This will hold all of the identifiers for a particular scope 
 typedef struct
 {
     Symbol * data;
@@ -21,6 +23,7 @@ typedef struct
     unsigned long hash_limit;
 } SymbolTable;
 
+// Constructor for a symbol
 Symbol init_symbol(ASTNode * node)
 {
     char * id = node->data.identifier;
@@ -28,66 +31,121 @@ Symbol init_symbol(ASTNode * node)
     char * datatype = NULL;
     char * key;
 
+    // Set symbol data type
     if(node->type == VARIABLE_DECL || node->type == FUNCTION_DECL)
         datatype = node->data.IdentifierData.type;
 
+    // Evaluate the key of the symbol
     if(node->type == FUNCTION_DECL)
         key = key_function(node);
     else if (node->type == VARIABLE_DECL)
         key = key_variable(node);
 
+    // Evaluate the hash
     unsigned long hash = hash(key, hash_limit);
 
+    // Construct the symbol
     Symbol symbol = (Symbol)malloc(sizeof(Symbol));
     symbol.identifier = strdup(id);
     symbol.type = strdup(type);
     symbol.datatype = strdup(datatype);
+    symbol.key = strdup(key);
+
+    return symbol;
 }
 
-SymbolTable init_symbol_table()
-{
-    SymbolTable table = (SymbolTable)malloc(sizeof(SymbolTable));
-    table.symbol_count = 0;
-    table.data = (Symbol*)malloc(sizeof(Symbol)*table.symbol_count);
 
-    table.hash_limit = 128;
+
+SymbolTable* init_symbol_table()
+{
+    SymbolTable* table = (SymbolTable*)malloc(sizeof(SymbolTable));
+    table->symbol_count = 0;
+    table->data = (Symbol*)malloc(sizeof(Symbol)*table.symbol_count);
+
+    table->hash_limit = 128;
 
     return table;
 }
 
-void append_symbol(SymbolTable table, Symbol symbol)
-{
-    table.symbol_count++;
-    table.data[symbol_count - 1] = symbol;
+void insert_symbol(SymbolTable *table, Symbol symbol) {
+    table->symbol_count++;
+    table->data[table->symbol_count - 1] = symbol;
 
     // Double hash limit if table runs out of space
-    if (table.symbol_count >= table.hash_limit)
-    {
-        table.hash_limit *= 2;
-        table.data = (Symbol*)realloc(sizeof(Symbol) * table.hash_limit);
+    while (table->symbol_count >= table->hash_limit / 2) {
+        table->hash_limit *= 2;
+
+        Symbol *tmp = (Symbol *)malloc(sizeof(Symbol) * table->hash_limit);
+
+        // Rehash all symbols
+        for (int i = 0; i < table->hash_limit / 2; i++) {
+            if (table->data[i] != NULL) {
+                int new_index = hash(table->data[i].key, table->hash_limit);
+
+                // Handle collisions by linear probing
+                while (tmp[new_index] != NULL) {
+                    new_index = (new_index + 1) % table->hash_limit;
+                }
+
+                tmp[new_index] = table->data[i];
+            }
+        }
+
+        free(table->data);
+        table->data = tmp;
     }
 }
 
-bool insert(SymbolTable table, char * key)
-{
-    unsigned long hash = hash(key, table.hash_limit)
-    
-    if (table.data[hash] != NULL)
-        return true;
+bool search_symbol(SymbolTable *table, char *key) {
+    int index = hash(key, table->hash_limit);
 
+    // Resolve collisions via linear probing
+    while (table->data[index] != NULL) 
+    {
+        if (strcmp(table->data[index]->key, key) == 0) {
+            return true
+        }
+        index = (index + 1) % table->hash_limit;
+    }
+
+    // If key is not found,
     return false;
 }
+unsigned long get_symbol_position(SymbolTable * table, char * key)
+{
+    unsigned long hash = hash(key, table->hash_limit);
+
+    int index = hash(key, table->hash_limit);
+
+    while (table->data[index] != NULL) 
+    {
+        if (strcmp(table->data[index]->key, key) == 0) 
+        {
+            return index;
+        }
+
+        index = (index + 1) % table->hash_limit;
+    }NULL
+
+    return NULL;
+}
+
 
 void delete_symbol(SymbolTable table, char * key)
 {
-    unsigned long hash = hash(key, table.hash_limit);
+    if(!search_symbol(table, key))
+        exit(EXIT_FAILURE);
+
+    
+    unsigned long index = get_symbol_position(table, key)
 
     //Free the symbol
-    free(table.data[hash - 1]);
+    free(table->data[index - 1]);
 
     //Clear the symbol
-    table.data[hash - 1] = NULL;
+    table->data[index - 1] = NULL;
 }
+
 
 char * key_function(ASTNode * node)
 {
@@ -125,4 +183,29 @@ char * key_variable(ASTNode * node)
         return key;
     }
     return NULL;
+}
+
+
+void free_symbol(Symbol *symbol) {
+    if (symbol) {
+        free(symbol->identifier);
+        free(symbol->type);
+        free(symbol->datatype);
+        free(symbol->key);
+        free(symbol);
+    }
+}
+
+void free_symbol_table(SymbolTable *table) {
+    if (table) {
+        // Free every symbol in the table
+        for (unsigned long i = 0; i < table->symbol_count; i++) {
+            if (table->data[i]) {
+                free_symbol(&(table->data[i])); 
+            }
+        }
+        //Free the table itelf
+        free(table->data); 
+        free(table);      
+    }
 }
